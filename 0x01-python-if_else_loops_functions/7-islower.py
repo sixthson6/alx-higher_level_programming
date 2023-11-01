a2ed3dad0e523feb006c2bea15495e24304f03c7
sixthson6 <sudais2408@gmail.com>
2023-10-31 20:12:53 -0700

    first functon

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    for i in range(ord('a'), ord('z') + 1):
-        if c == i:
+    if ord(c) >= 97 and ord(c) <= 122:
             return (True)
         else:
             return (False)
