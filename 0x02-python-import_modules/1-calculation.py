bcdf9873afb5037d9f6d1325fd8716086adf6900
sixthson6 <sudais2408@gmail.com>
2023-11-02 01:01:24 -0700

    1-calculation.py added

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+_add = add(a, b)
+_sub = sub(a, b)
+_mul = mul(a, b)
+_div = div(a, b)
+
+print("{} + {} = {}".format(a, b, _add))
+print("{} - {} = {}".format(a, b, _sub))
+print("{} * {} = {}".format(a, b, _mul))
+print("{} / {} = {}".format(a, b, _div))
