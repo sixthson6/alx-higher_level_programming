73cc847ef4275d3922f4985ee8383aa93bdcbf9c
sixthson6 <sudais2408@gmail.com>
2023-11-02 00:50:10 -0700

    import add()

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from add_0 import add
+
+result = add(1, 2)
+
+print("1 + 2 = {}".format(result))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+All about importing modules and using them in
+other python scripts
