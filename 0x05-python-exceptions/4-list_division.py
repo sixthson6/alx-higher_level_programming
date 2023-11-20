6c2efbc31f779bfbb319eb2af2b7522612ae0171
sixth_son6 <sudais2408@gmail.com>
2023-11-20 15:58:51 -0800

    4-list_division.py added

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,16 +5,16 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
             if result != result or result == float('inf') or result == float('-inf'):
-                resul = 0.0
+                resul = 0
         except IndexError:
             print("out of range")
-            result = 0.0
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            result = 0.0
+            result = 0
         except TypeError:
             print("wrong type")
-            result = 0.0
+            result = 0
         my_list_3.append(result)
     return my_list_3
 
