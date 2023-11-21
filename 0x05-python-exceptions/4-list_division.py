922e41abb56ec2d88004cac7ef804a3973b76034
sixth_son6 <sudais2408@gmail.com>
2023-11-20 16:00:25 -0800

    4-list_division.py added

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,6 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             result = 0
-        my_list_3.append(result)
+        finally:
+            my_list_3.append(result)
     return my_list_3
 
