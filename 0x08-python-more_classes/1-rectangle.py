792079f518ef0d157ad5691b398936fa7cc273b8
sixthson6 <sudais2408@gmail.com>
2023-11-30 01:35:29 +0000

    1-rectangle.py added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
-        if type(value) not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -30,7 +30,7 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        if type(value) not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
