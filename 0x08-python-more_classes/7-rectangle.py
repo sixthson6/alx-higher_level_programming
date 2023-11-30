ccf70bd0beedaede37284f1cb030404fb8a9fab0
sixthson6 <sudais2408@gmail.com>
2023-11-30 03:19:47 +0000

    7-rectangle.py added

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,7 +68,7 @@ class Rectangle():
 
         rectangle = []
         for i in range(self.__height):
-            [rectangle.append("#") for j in range(self.__width)]
+            [rectangle.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rectangle.append("\n")
         return ("".join(rectangle))
