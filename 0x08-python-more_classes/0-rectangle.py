29384c3256b1feec71245e2ac46bda443e136892
sixthson6 <sudais2408@gmail.com>
2023-11-30 00:37:11 +0000

    an empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class definition of a Rectangle
+"""
+class Rectangle():
+    """
+    An empty class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+classes
+OOP
