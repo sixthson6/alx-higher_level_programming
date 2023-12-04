281ed397852c8d294d67332042229953625da794
sixthson6 <sudais2408@gmail.com>
2023-12-04 22:24:36 +0000

    list all attributes and methods of a class

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,16 @@
     This module contains a function to list all
     available attributes of any given object
 """
+
+
 def lookup(obj):
+    """
+    function to list attributes and method of a class
+
+    Parameters:
+        @obj
+
+    Return:
+         a list of all attributes and methods of a class
+    """
     return dir(obj)
