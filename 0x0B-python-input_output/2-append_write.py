42c9fb5e94d7d765af1f0fb7756813cb9c64e8a3
sixthson6 <sudais2408@gmail.com>
2023-12-05 21:36:36 +0000

    append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this module contains a function that appends
+text to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    this file contains a file that appends text to 
+    an already existing file
+
+    Parameters:
+        @filename: file to be written to
+        @text: text to append
+
+    Return:
+        number of chars added
+    """
+    with open(filename, 'a', encoding='utf-8') as fd:
+        bytes_appended = fd.write(text)
+    return bytes_appended
