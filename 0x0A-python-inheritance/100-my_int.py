2847e1045d4babb2e955064abfd0887ea5ad9f36
sixthson6 <sudais2408@gmail.com>
2023-12-05 00:47:00 +0000

    invert equal sign

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module contains a class """
+
+
+class MyInt(Int):
+    """ inverts equal sign operator """
+    def __eq__(self, value):
+        return self.real != value
+
+    def __ne__(self, value):
+        return self.real == value
