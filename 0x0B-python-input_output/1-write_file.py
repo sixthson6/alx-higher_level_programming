bebe322b27602b4c307ca88c623836298fb0492f
sixthson6 <sudais2408@gmail.com>
2023-12-05 21:27:21 +0000

    write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" this module contains a funtion to write to a file"""
+
+
+def write_file(filename="", text=""):
+    """
+    this function writes to a file
+
+    Parameters:
+        @filename: name of file
+        @text: input text
+
+    Return: number of bytes written
+    """
+    with open(filename, 'w', encoding='utf-8') as fd:
+        bytes_written =fd.write(text)
+    return bytes_written
