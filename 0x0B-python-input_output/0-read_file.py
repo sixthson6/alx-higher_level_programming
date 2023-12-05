8656ab2654cb6bbf1e79829cad67ea8adf35149f
sixthson6 <sudais2408@gmail.com>
2023-12-05 21:11:53 +0000

    read files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,15 @@
 
 
 def read_file(filename=""):
+    """
+    function to open file
+
+    Parameters:
+        @filename: filename to be opened
+
+    Return:
+        contents of file
+    """
     with open(filename, 'r', encoding='utf-8') as fd:
         file_content = fd.read()
         print(file_content)
