254925d2dc4205b2a32e988640816241534faed0
sixthson6 <sudais2408@gmail.com>
2023-12-05 23:34:46 +0000

    script to add to json file from cmd

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this module contains a scritpt to load and
+save a json file from command line arguments
+"""
+import sys
+
+
+if __name__ == '__main__':
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        objs = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        objs = []
+    objs.extend(sys.argv[1:])
+    save_to_json_file(objs, "add_item.json")
