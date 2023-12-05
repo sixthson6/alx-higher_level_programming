07dcd9313982b99cdfc796cc0326fa5158f8148d
sixthson6 <sudais2408@gmail.com>
2023-12-05 22:26:33 +0000

    serialize and save to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this module contains a function that obj to a text
+file using json representaion
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    function to write an obj file to a text file.
+    an obj using json representation
+
+    Parameter:
+        @my_obj: python obj
+        @filename: file to write to
+    """
+    import json
+    with open(filename, 'w', encoding='utf-8') as fd:
+        json.dump(my_obj, fd)
+        
