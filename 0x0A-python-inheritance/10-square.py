a7af7b6b353f348f6f99f3d6b28a3224564c899c
sixthson6 <sudais2408@gmail.com>
2023-12-05 00:30:30 +0000

    sqare inherit rect

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+this module only contains a class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    this class inherits Rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
