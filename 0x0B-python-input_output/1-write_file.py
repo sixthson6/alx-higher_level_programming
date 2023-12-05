5d709d82fd446c727a3e5f1af80480255ccd5cfe
sixthson6 <sudais2408@gmail.com>
2023-12-05 21:28:08 +0000

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,5 +13,5 @@ def write_file(filename="", text=""):
     Return: number of bytes written
     """
     with open(filename, 'w', encoding='utf-8') as fd:
-        bytes_written =fd.write(text)
+        bytes_written = fd.write(text)
     return bytes_written
