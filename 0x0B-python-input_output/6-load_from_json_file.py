775a9deacaa437a1984871b8e073832d72b39772
sixthson6 <sudais2408@gmail.com>
2023-12-05 22:37:47 +0000

    load form a json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this module contains a function that
+creates an obj from a json file
+"""
+
+
+def load_from_json_file(filename):
+    """
+    load data form a json file
+
+    Parameter:
+        @filename: json file
+
+    """
+    import json
+    with open(filename, 'r', encoding='utf-8') as fd:
+        json.load(fd)
