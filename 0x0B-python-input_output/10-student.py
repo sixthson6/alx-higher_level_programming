315dde0f8e5273d967735a5aca9f771024c74da8
sixthson6 <sudais2408@gmail.com>
2023-12-07 02:13:03 +0000

    student class_2

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this module contains a class
+"""
+
+
+class Student:
+    """
+    this class describes a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieve dictionary repr of instance
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return vars(self)
