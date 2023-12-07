5b7929549027992a5710bdebcab416714a66ca38
sixthson6 <sudais2408@gmail.com>
2023-12-07 01:38:20 +0000

    vars()

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function to return a simple data
+structure for a json serialization of an obj
+"""
+
+
+def class_to_json(obj):
+    """
+    function to return dictionary description for json
+    serialization
+
+    Parameter:
+        @obj: instance of a class
+
+    Return:
+        dictionary description
+    """
+    items = vars(obj)
+    return items
+   
