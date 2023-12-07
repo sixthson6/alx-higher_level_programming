dd10cc40166eeabda281fbadb7ac47a2a67a2941
sixthson6 <sudais2408@gmail.com>
2023-12-07 02:36:33 +0000

    update json string from class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this module contains a class
+"""
+
+
+class Student:
+    """
+    this class describes a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieve dictionary repr of instance
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return vars(self)
+
+    def reload_from_json(self, json):
+        """
+        replace attributes
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
