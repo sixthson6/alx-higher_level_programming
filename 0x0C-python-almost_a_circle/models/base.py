6a15ef74140ce552263c6e3b4778b929f4a6ed08
sixthson6 <sudais2408@gmail.com>
2023-12-14 00:55:40 +0000

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this module defines a class 'Base'"""
+
+
+class Base:
+    """this is a base class created to be
+    inherited by other classes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
