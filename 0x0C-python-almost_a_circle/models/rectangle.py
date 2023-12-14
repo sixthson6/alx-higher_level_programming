4f722fb12d1c75fb0590d091ac45cc37487481ba
sixthson6 <sudais2408@gmail.com>
2023-12-14 03:06:00 +0000

    rectange update args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,3 +137,22 @@ class Rectangle(Base):
                                                          self.__width,
                                                          self.__height)
         return string
+
+    def update(self, *args):
+        """
+        update the instance attributes
+
+        width
+        height
+        x
+        y
+        """
+        self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
