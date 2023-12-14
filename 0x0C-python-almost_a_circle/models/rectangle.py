76ae2fd71dc29fc8098d53e28997e85e9469a99f
sixthson6 <sudais2408@gmail.com>
2023-12-14 13:46:41 +0000

    rectange update kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -138,7 +138,7 @@ class Rectangle(Base):
                                                          self.__height)
         return string
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         update the instance attributes
 
@@ -147,12 +147,16 @@ class Rectangle(Base):
         x
         y
         """
-        self.id = args[0]
-        if len(args) > 1:
-            self.__width = args[1]
-        if len(args) > 2:
-            self.__height = args[2]
-        if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+        if len(args) != None:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        for k, v in kwargs.items():
+            setattr(self, k, v)
