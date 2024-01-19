6a20487a4e87614521455f58bac6d9d58bd868c4
sixthson6 <sudais2408@gmail.com>
2024-01-19 19:26:54 +0000

    top 3 cities with high tmp in july n aug

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- list top 3 cities with highest temp in july and august
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
