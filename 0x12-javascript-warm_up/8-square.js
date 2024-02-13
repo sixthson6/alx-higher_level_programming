85677eb32bd8272c06ee2b50440204757226c8f2
sixthson6 <sudais2408@gmail.com>
2024-02-13 18:57:29 +0000

    Add script that prints square based on input arg

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const num = process.argv[2];
 
-if (process.argv.length < 3) {
+if (process.argv.length < 3 && !(Number.isInteger(Number(num))) && Number(num) === num) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
