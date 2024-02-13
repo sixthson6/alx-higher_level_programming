2598afb65a5746812b00bc09ceb10370029f7e60
sixthson6 <sudais2408@gmail.com>
2024-02-13 18:49:34 +0000

    Add script to print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,8 @@ const num = process.argv[2];
 
 if (process.argv.length < 3) {
   console.log('Missing size');
-}
-for (let i = 0; i < num; i++) {
-  console.log('X'.repeat(num));
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 }
