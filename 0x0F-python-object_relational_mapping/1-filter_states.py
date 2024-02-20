d2856bd3164cd9d2963bf3c2864fe3be38a15dd5
sixthson6 <sudais2408@gmail.com>
2024-02-20 15:56:02 +0000

    Add sql query to filter states with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains code to connect to a database
+"""
+import MySQLdb
+import sys
+
+if '__name__' == '__main__':
+    """List all states from database 'tbtn_0e_0_usa'
+    """
+
+    cnx = MySQLdb.connect(host=sys.argv[1], user=sys.argv[2], db=sys.argv[3])
+    cursor = cnx.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%'"
+    cursor.execute(query)
+    [print(state) for state in cursor.fetchall()]
