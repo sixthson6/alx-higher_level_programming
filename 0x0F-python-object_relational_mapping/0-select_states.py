1c0cdb8c1868b72d14381820a5e7e9116c044bab
sixthson6 <sudais2408@gmail.com>
2024-02-20 16:00:50 +0000

    Add first script to manipulate relational database using database connector (MySQLdb)

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,13 @@ if '__name__' == '__main__':
     """List all states from database 'tbtn_0e_0_usa'
     """
 
-    cnx = MySQLdb.connect(host=sys.argv[1], user=sys.argv[2], db=sys.argv[3])
+    cnx = MySQLdb.connect(
+            host=localhost,
+            port=3306,
+            user=sys.argv[1],
+            user=sys.argv[2],
+            db=sys.argv[3]
+    )
     cursor = cnx.cursor()
     query = "SELECT * FROM states"
     cursor.execute(query)
