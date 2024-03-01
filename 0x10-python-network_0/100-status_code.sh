9d2f87adb3613e86abecb3c2246c1d788246f65b
sixthson6 <sudais2408@gmail.com>
2024-02-29 18:38:03 -0800

    return status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display status code
-curl -sI "$1" | awk '/HTTP\/1.1/ {print $2}' | tr -d '\n'
+curl -sw "%{http_code}" -o /dev/null "$1" # curl -sI "$1" | awk '/HTTP\/1.1/ {print $2}' | tr -d '\n'
