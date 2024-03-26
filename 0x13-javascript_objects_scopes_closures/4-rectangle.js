1a49e91de03dc0be2868ac1d50c2a4e8ce1b11ce
sixthson6 <sudais2408@gmail.com>
2024-03-26 07:36:39 +0000

    Add additional rectangle methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,19 +9,19 @@ module.exports = class Rectangle {
   }
 
   print () {
-	  for (let i = 0; i < this.height; i++) {
-		  console.log('X'.repeat(this.width));
-	  }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 
   rotate () {
-	  const temp = this.witdth;
-	  this.width = this.height;
-	  this.height = temp;
+    const temp = this.witdth;
+    this.width = this.height;
+    this.height = temp;
   }
 
   double () {
-	  this.width = this.widht * 2;
-	  this.height = this.height * 2;
+    this.width = this.widht * 2;
+    this.height = this.height * 2;
   }
 };
