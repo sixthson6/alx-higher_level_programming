07d07023104ec1ef5685735a3c42b48c7fb49eba
sixthson6 <sudais2408@gmail.com>
2024-03-26 07:29:32 +0000

    Add additional rectangle methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// Rectangle with arg validation
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+	  for (let i = 0; i < this.height; i++) {
+		  console.log('X'.repeat(this.width));
+	  }
+  }
+
+  rotate () {
+	  const temp = this.witdth;
+	  this.width = this.height;
+	  this.height = temp;
+  }
+
+  double () {
+	  this.width = this.widht * 2;
+	  this.height = this.height * 2;
+  }
+};
