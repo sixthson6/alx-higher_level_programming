45c3a9616a1b8fb5a8e47d471472d812e4050ff0
sixthson6 <sudais2408@gmail.com>
2024-03-26 07:58:20 +0000

    Add sqaure class inherit other square display square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//Square class
+const Squared = require('./5-square');
+
+module.exports = class Square extends Squared {
+	charPrint(c) {
+		if (c === undefined) {
+		this.print();
+		}
+		for (let i = 0; i < this.width; i++) {
+			console.log("c".repeat(this.height));
+		}
+	}
+};
