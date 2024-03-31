0852a31403fbfba9407d8a01392e2e1f8841bf03
sixthson6 <sudais2408@gmail.com>
2024-03-31 09:19:05 -0700

    list all states using mysqldb adaptor

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,18 +4,20 @@
 import MySQLdb
 import sys
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     """List all states from database 'tbtn_0e_0_usa'
     """
 
     cnx = MySQLdb.connect(
-            host=localhost,
+            host="localhost",
             port=3306,
             user=sys.argv[1],
-            user=sys.argv[2],
+            passwd=sys.argv[2],
             db=sys.argv[3]
     )
     cursor = cnx.cursor()
     query = "SELECT * FROM states"
     cursor.execute(query)
-    [print(state) for state in cursor.fetchall()]
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
