f0c9e59fb4be90824e82b94037f3f448cbaf5723
sixthson6 <sudais2408@gmail.com>
2024-04-24 15:28:42 +0000

    script writes the contents of a webpage into a file locally

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,12 +7,12 @@ const url = process.argv[2];
 const file = process.argv[3];
 
 request(url, (error, response, body) => {
-	if (!error && response.statusCode === 200) {
-		/*const siteData = JSON.parse(body);*/
-		fs.writeFile(file, body, 'utf8', (error) => {
-			if (error) {
-				console.error('Error:', error);
-			}
-		});
-	};
+  if (!error && response.statusCode === 200) {
+    /* const siteData = JSON.parse(body); */
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.error('Error:', error);
+      }
+    });
+  }
 });
