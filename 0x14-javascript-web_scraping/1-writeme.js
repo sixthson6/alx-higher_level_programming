e5fc50d83cc9cfe8fcd064e2af8945aac60c5997
sixthson6 <sudais2408@gmail.com>
2024-04-24 00:11:00 +0000

    script writes to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(file, data, 'utf8', (error) => {
+	if (error) {
+		console.error(error);
+	}
+});
