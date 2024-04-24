21cbc292edfbd9df1cca9d1e48fe68907bef5367
sixthson6 <sudais2408@gmail.com>
2024-04-24 22:55:29 +0000

    scipt finds number of completed task from a json (requested from a webpage)

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
-const file = process.argv[3];
 
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
