586cba84ed9367a8dc234a13916ec0ea2ae5601f
sixthson6 <108383344+sixthson6@users.noreply.github.com>
2024-07-30 15:03:58 +0000

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const path = process.argv[2];
-
-request(path, (error, response, body) => {
-  console.log('code:', response && response.statusCode);
-});
--- 0x14-javascript-web_scraping/README.md
+++
@@ -1,1 +0,0 @@
-JavaScript for web-scraping
